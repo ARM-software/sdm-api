834390e55d91a6bc26c3f66ca392425aa822df14
Chris Reed <chris.reed@arm.com>
2022-01-26 16:09:59 -0600

    API: update copyright year for 2022.

--- include/secure_debug_manager.h
+++ include/secure_debug_manager.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021 Arm Ltd
+ * Copyright (c) 2020-2022 Arm Ltd
  * SPDX-License-Identifier: BSD-3-Clause
  *
  * Redistribution and use in source and binary forms, with or without modification,
